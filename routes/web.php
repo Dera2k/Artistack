35d19d2cf7cbaad8941c40c66a75691b160e824e
Chidera Nwogu <chideranwogu2003@gmail.com>
2025-07-11 20:50:47 +0100

    initial commit

---
+++ routes/web.php
@@ -0,0 +1,19 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\artworkController;
+use App\Http\Controllers\aboutController;
+
+Route::get('/', function () {
+    return ('welcome');
+});
+
+// home page route
+Route::get("/", [HomeController::class, 'index'])->name('home');
+
+// artwork illustrations page route
+Route::get("/artworks", [ArtworkController::class, 'index'])->name('artworks');
+
+// about page route
+Route::get("/about", [AboutController::class, 'index'])->name('about');
