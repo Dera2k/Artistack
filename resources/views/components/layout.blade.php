c9eae12578bcdf85d402a466537bb3f1c1f5ad67
Chidera Nwogu <chideranwogu2003@gmail.com>
2025-07-24 21:12:05 +0100

    created a nav link component

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -3,13 +3,13 @@
 
 <head>
     <meta charset="UTF-8">
-    <title>{{$title ?? 'Artistack'}}</title>
+    <title>{{ $title ?? 'Artistack' }}</title>
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 </head>
 
 <body class="bg-gray-200 ">
     <x-header />
-        <div class="flex">
+    <div class="flex">
         <main class="w-full flex-grow min-h-[80vh]: mx-auto py-8">
             {{$slot}} <!-- main content here -->
         </main>
