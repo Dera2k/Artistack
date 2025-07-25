a4aa51e98dff9431bc54a891602608c07627d6a7
Chidera Nwogu <chideranwogu2003@gmail.com>
2025-07-25 21:33:11 +0100

    added a plus symbol to add art, - takes to artworks.create page

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -1,17 +1,30 @@
+
+@php use Illuminate\Support\Str; @endphp
 <header class="bg-blue-700 border-10 border-black-900 w-full z-50">
     <div class="mx-auto flex justify-between py-4 px-5">
         <a href="{{ route('home') }}" class="text-white font-bold text-lg">Artistack</a>
         <nav>
             <ul class="flex space-x-7 text-white  font-bold text-lg">
                 <li>
-                    <a href="{{ route('home') }}" class="hover:text-orange-400 hover:underline {{request()->is('/') ? "text-orange-400 font-bold": " "}}">Home
+                    @if(Str::startsWith(Route::currentRouteName(),'artworks.'))
+            <a href="{{route ('artworks.create') }}"
+                class="ml-4 bg-orange-400 text-white text-xl font-bold px-3 py-1 rounded-full">
+                +
+                </a>
+            @endif
+            
+                </li>
+                <li>
+                    <a href="{{ route('home') }}" class="hover:text-orange-400 hover:underline">Home
                     </a>
                 </li>
                 <li>
-                    <a href="{{ route('artworks.index') }}" class="hover:text-orange-400 hover:underline {{request()->is('artworks*') ? "text-orange-400" : " "}}">Artworks
+                    <a href="{{ route('artworks.index') }}" class="hover:text-orange-400 hover:underline">Artworks
                     </a>
                 </li>
             </ul>
+
+            
         </nav>
     </div>
 </header>
