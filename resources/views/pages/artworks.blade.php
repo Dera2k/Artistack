71ffc9efc9e11cd8cfd0b848040f36537e8e4fde
Chidera Nwogu <chideranwogu2003@gmail.com>
2025-07-28 14:14:36 +0100

    added logic viewing to the artwork page, if no art display no artworks, if 1 or 2 it should be front and centre but if 3 it should be in a gride view with 3 cols

--- resources/views/pages/artworks.blade.php
+++ resources/views/pages/artworks.blade.php
@@ -1,21 +1,39 @@
 <x-layout title="Artworks">
-    <section class="container mx-auto px-4 pt-2">
 
+    <section class="mx-auto px-4 pt-2">
         <h1 class="text-2xl font-bold mb-6 text-center text-black">My Artworks</h1>
-           <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6 max-w-5xl">
-                <!-- Card 1 -->
+
+        @if($artworks->isEmpty())
+            <p class="text-center ">No artworks available</p>
+        @else
+
+        @php
+            $count = $artworks->count();
+            $grid_classes = 'grid gap-6 max-w-5xl mx-auto';
+            if ($count ===1)
+            {
+                $grid_classes .= 'grid-cols-1 justify center'; 
+            }
+            elseif ($count ===2)
+            {
+                $grid_classes .= 'grid-cols-1 sm:grid-cols-2 justify-center';
+            }
+            else
+            {
+                $grid_classes .= 'grid-cols-1 sm:grid-cols-2 md:grid-cols-3 justify-center'; 
+            }
+        @endphp
+        <div class="{{ $grid_classes }}">
+            @foreach ($artworks as $artwork)
                 <div class="bg-white shadow-md rounded-lg overflow-hidden">
-                    {{-- <img src="{{ asset('images/art1.jpg') }}" alt="Artwork 1" class="w-full h-48 object-cover"> --}}
+                    <img src="{{ asset('storage/'.$artwork->image_path)}}" alt="{{ $artwork->title }}">
                     <div class="p-4">
-                        @forelse ($artworks as $artwork)
-                        <h2 class="text-lg font-semibold mb-2">{{$artwork->title}}</h2>
-                        <p class="text-gray-600 text-sm">{{$artwork->description}}</p>
-                        @empty
-                        <p>No art available</p>
-                        @endforelse
+                        <h2>{{ $artwork->title}}</h2>
+                        <p>{{ $artwork->description}}</p>
                     </div>
                 </div>
-                <!-- Add more cards here if needed -->
-            </div>
+            @endforeach    
+        </div>
+        @endif
     </section>
 </x-layout>
