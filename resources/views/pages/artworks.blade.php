6ebe027f4fe9ef66f9083bd1166bcedcc3fc976c
Chidera Nwogu <chideranwogu2003@gmail.com>
2025-07-30 21:58:02 +0100

    fixed the layout of the cards on the artworks page

--- resources/views/pages/artworks.blade.php
+++ resources/views/pages/artworks.blade.php
@@ -9,24 +9,25 @@
 
         @php
             $count = $artworks->count();
-            $grid_classes = 'grid gap-6 max-w-5xl mx-auto';
+            $grid_classes = 'grid gap-8 max-w-5xl mx-auto';
             if ($count ===1)
             {
-                $grid_classes .= 'grid-cols-1 justify center'; 
+                $grid_classes .= ' grid-cols-1 place-content-center'; 
             }
             elseif ($count ===2)
             {
-                $grid_classes .= 'grid-cols-1 sm:grid-cols-2 justify-center';
+                $grid_classes .= ' grid-cols-1 sm:grid-cols-2 place-content-center';
             }
             else
             {
-                $grid_classes .= 'grid-cols-1 sm:grid-cols-2 md:grid-cols-3 justify-center'; 
+                $grid_classes .= ' grid-cols-1 sm:grid-cols-2 md:grid-cols-3 place-content-center'; 
             }
         @endphp
         <div class="{{ $grid_classes }}">
             @foreach ($artworks as $artwork)
                 <div class="bg-white shadow-md rounded-lg overflow-hidden">
-                    <img src="{{ asset('storage/'.$artwork->image_path)}}" alt="{{ $artwork->title }}">
+                    <img src="{{ asset('storage/'.$artwork->image_path)}}" alt="{{ $artwork->title }}" 
+                    class="w-full h-72 mx-auto object-cover">
                     <div class="p-4">
                         <h2>{{ $artwork->title}}</h2>
                         <p>{{ $artwork->description}}</p>
