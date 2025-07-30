490524bd70f5f2588bb55a93b945cf0ad6d1c522
Chidera Nwogu <chideranwogu2003@gmail.com>
2025-07-30 21:48:10 +0100

    added font awesome cdn link

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -5,6 +5,7 @@
     <meta charset="UTF-8">
     <title>{{ $title ?? 'Artistack' }}</title>
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 </head>
 
 <body class="bg-gray-200 ">
