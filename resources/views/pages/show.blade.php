ed0720c8ab7ad608dfd69395bb70d08539fa5bb0
Chidera Nwogu <chideranwogu2003@gmail.com>
2025-08-04 10:16:35 +0100

    worked on the show.blade.php page, might revisit

--- resources/views/pages/show.blade.php
+++ resources/views/pages/show.blade.php
@@ -1,7 +1,7 @@
 <x-layout title="{{ $artwork -> title }}">
     <section class="max-w-3xl mx-auto px-4 py-8">
         <div class="bg-white rounded-lg shadow-md overflow-hidden">
-            <img src="{{ asset('storage/' .$artwork->image_path) }}" alt="{{ $artwork->title }}"
+            <img src="{{ asset('storage/'.$artwork->image_path) }}" alt="{{ $artwork->title }}"
             class="w-full h-96 object-cover">
 
             <div class="p-6">
