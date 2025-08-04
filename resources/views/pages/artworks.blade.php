e74337976a63716963fd87f69eb4941a5c35ee09
Chidera Nwogu <chideranwogu2003@gmail.com>
2025-08-04 09:53:24 +0100

    wrapped the art cards in a a href link within the foreach loop to redirect to the show.blade.php,

--- resources/views/pages/artworks.blade.php
+++ resources/views/pages/artworks.blade.php
@@ -25,28 +25,29 @@
         @endphp
         <div class="{{ $grid_classes }}">
             @foreach ($artworks as $artwork)
-                <div class="bg-white shadow-md rounded-lg overflow-hidden">
-                    <img src="{{ asset('storage/'.$artwork->image_path)}}" alt="{{ $artwork->title }}" 
-                    class="w-full h-72 mx-auto object-cover">
+                <a href="{{ route('artworks.show', $artwork -> id) }}" 
+                    class="block hover: shadow-xl transition">
+                    <div class="bg-white shadow-md rounded-lg overflow-hidden">
+                        <img src="{{ asset('storage/'.$artwork->image_path)}}" alt="{{ $artwork->title }}" 
+                        class="w-full h-72 mx-auto object-cover">
 
-                    <div class="p-4">
-                        <div class="flex justify-between items-start">
-                            <div>
-                                <h2>{{ $artwork->title}}</h2>
-                                <p>{{ $artwork->description}}</p>
+                        <div class="p-4">
+                            <div class="flex justify-between items-start">
+                                <div>
+                                    <h2>{{ $artwork->title}}</h2>
+                                    <p>{{ $artwork->description}}</p>
+                                </div>
+                                <form action="{{ route('artworks.destroy', $artwork->id) }}" method="POST" onsubmit="return confirm('Are you sure you want to delete this artwork?');">
+                                    @csrf
+                                    @method('delete')
+                                    <button type="submit" class="text-red-500 hover:text-red-700 ml-3" title="Delete">
+                                        <i class="fas fa-trash"></i>
+                                    </button>
+                                </form>
                             </div>
-                            <form action="{{ route('artworks.destroy', $artwork->id) }}" method="POST" onsubmit="return confirm('Are you sure you wantto delete this artwork?');">
-                                @csrf
-                                @method('delete')
-                                <button type="submit" class="text-red-500 hover:text-red-700 ml-3" title="Delete">
-                                    <i class="fas fa-trash"></i>
-                                </button>
-                            </form>
-
-
                         </div>
                     </div>
-                </div>
+                </a>
             @endforeach    
         </div>
         @endif
