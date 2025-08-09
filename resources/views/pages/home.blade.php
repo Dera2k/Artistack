9258dd0f246b333ec83cae859ff96cce1967cf42
Chidera Nwogu <chideranwogu2003@gmail.com>
2025-08-09 22:26:14 +0100

    struggled, made some changes to files, and got vite setup

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -1,5 +1,5 @@
 <x-layout>
-<div class="flex flex-col items-center justify-center bg-blue-400 text-white h-[70vh] space-y-4">
+<div class="flex flex-col items-center justify-center bg-blue-600 text-white h-[70vh] space-y-4">
         <h1 class="text-4xl font-bold">WELCOME</h1>
         <h1 class="text-4xl font-bold">TO</h1>
         <h1 class="text-6xl font-bold">ARTISTACK</h1>
