9aa4b0c3e229d1185d91d6b114456a61e83244cd
Chidera Nwogu <chideranwogu2003@gmail.com>
2025-08-11 21:18:00 +0100

    created the edit form for the edit title and description feature

--- resources/views/pages/edit.blade.php
+++ resources/views/pages/edit.blade.php
@@ -1,3 +1,27 @@
 <x-layout>
-    
+    <div class="flex-col items-center justify-center min-h-screen">
+        <h1 class="text-2xl font-bold text-center text-transform: uppercase">Edit Details</h1>
+        <form action="{{ route('artworks.update', $artwork->id) }}" method="POST" 
+            class="bg-white rounded-lg p-12 mt-9">
+            @csrf
+
+            <div class="mb-6">
+                <label for="title" class="block ">Title:</label>
+                <input type="text" name="title" value="{{ old('title') }}" placeholder="Title" required
+                class="w-full-border border-gray-800 rounded p-3 focus:outline-none focus:ring-2 focus:ring-blue-400 shadow-sm">
+            </div>
+
+            <div>
+                <label for="description" class="block mb-2">Description</label>
+                <textarea name="description" rows="3" required 
+                class="w-full-border border-gray-500 rounded p-3 focus:outline-none focus:ring-2 resize-none">
+            {{ old('description') }}</textarea>
+            </div>
+
+            <div class="flex jistify-center mt-4">
+                <button type="submit"
+                class="bg-blue-500 text-white font-semibold px-6 py-2 rounded-lg hover:bg-blue-600 transition">Change</button>
+            </div>
+        </form>
+    </div>
 </x-layout>
